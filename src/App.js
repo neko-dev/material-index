0c2dcadd601d6490f1246549479f37deed7cc2df
Viosey <imviosey@gmail.com>
2017-09-07 01:29:48 +0800

    feat(UI): improve jumbotron & some styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import $ from 'jquery';
 import './App.css';
 
+
 class App extends Component {
+  componentDidMount() {
+    $(window).scroll(function (event) {
+      var scroll = $(window).scrollTop();
+      if (scroll > 40) {
+        $('.App-header').addClass('header-docked');
+      } else {
+        $('.App-header').removeClass('header-docked');
+      }
+    });
+  }
   render() {
     return (
       <div className="App">
@@ -13,20 +24,31 @@ class App extends Component {
               <div className="slogan">Theme for everyone</div>
             </div>
             <ul className="navbar-right clearfix">
-              <li className="nav-item">Demo</li>
-              <li className="nav-item">Docs</li>
-              <li className="nav-item">Github</li>
+              <li className="nav-item"><a href="#">Demo</a></li>
+              <li className="nav-item"><a href="#">Docs</a></li>
+              <li className="nav-item"><a href="#">Github</a></li>
             </ul>
           </div>
         </div>
 
-        <div className="jumbotron"></div>
+        <div className="jumbotron">
+          <h1>
+            <span>Nature, Pure</span>
+            <span>原质，纯粹</span>
+          </h1>
+          <div>
+            <a href="#" className="download-link">Get Material Free</a>
+          </div>
+        </div>
 
         <div className="presentation">
           <img src={require('./img/render_phone.png')} />
         </div>
 
         <div className="main">
+        </div>
+
+        <div className="footer">
 
         </div>
       </div>
