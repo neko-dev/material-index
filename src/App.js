c81d4e81ea8a07ba5f3d97da51dd8fe2dbbd06ff
Viosey <imviosey@gmail.com>
2017-09-24 15:00:57 +0800

    feat(UI): improve responsive design

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import LazyLoad from 'react-lazyload';
 import $ from 'jquery';
 import './App.css';
 
@@ -47,78 +48,78 @@ class App extends Component {
 
         <div className="main">
           <div className="advantage clearfix">
-            <div className="item-wrap">
-              <div className="item">
-                <img src="https://alimg.viosey.com/mt/advantage/seo.jpg" alt="[搜索引擎优化]" />
-                <div className="description">
-                  <h4>搜索引擎优化 (SEO)</h4>
-                  <p>
-                    <span>Meta tag，结构化数据，OpenGraph...</span>
-                    <span>满足搜索引擎优化需要</span>
-                  </p>
+              <div className="item-wrap">
+                <div className="item">
+                  <img src="https://alimg.viosey.com/mt/advantage/seo.jpg" alt="[搜索引擎优化]" />
+                  <div className="description">
+                    <h4>搜索引擎优化 (SEO)</h4>
+                    <p>
+                      <span>Meta tag，结构化数据，OpenGraph...</span>
+                      <span>满足搜索引擎优化需要</span>
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="item-wrap">
-              <div className="item">
-                <img src="https://alimg.viosey.com/mt/advantage/fast.jpg" alt="[优异的加载性能]" />
-                <div className="description">
-                  <h4>优异的加载性能</h4>
-                  <p>
-                    <span>Pagespeed 得分 A (100%)，</span>
-                    <span>Yslow 得分 A (90±5%)</span>
-                  </p>
+              <div className="item-wrap">
+                <div className="item">
+                  <img src="https://alimg.viosey.com/mt/advantage/fast.jpg" alt="[优异的加载性能]" />
+                  <div className="description">
+                    <h4>优异的加载性能</h4>
+                    <p>
+                      <span>Yslow 得分 A (90±5%)，</span>
+                      <span>Pagespeed 得分 A (100%)</span>
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="item-wrap">
-              <div className="item">
-                <img src="https://alimg.viosey.com/mt/advantage/function.jpg" alt="[众多功能]" />
-                <div className="description">
-                  <h4>众多功能</h4>
-                  <p>
-                    <span>两种代码高亮插件，四种独立页面，</span>
-                    <span>主题自带 CDN</span>
-                  </p>
+              <div className="item-wrap">
+                <div className="item">
+                  <img src="https://alimg.viosey.com/mt/advantage/function.jpg" alt="[众多功能]" />
+                  <div className="description">
+                    <h4>众多功能</h4>
+                    <p>
+                      <span>两种代码高亮插件，四种独立页面，</span>
+                      <span>主题自带 CDN...</span>
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="item-wrap">
-              <div className="item">
-                <img src="https://alimg.viosey.com/mt/advantage/lang.jpg" alt="[多种语言支持]" />
-                <div className="description">
-                  <h4>多种语言支持</h4>
-                  <p>
-                    <span>感谢国际友人的贡献，</span>
-                    <span>Material 现已支持 10 种语言</span>
-                  </p>
+              <div className="item-wrap">
+                <div className="item">
+                  <img src="https://alimg.viosey.com/mt/advantage/lang.jpg" alt="[多种语言支持]" />
+                  <div className="description">
+                    <h4>多种语言支持</h4>
+                    <p>
+                      <span>感谢国际友人的贡献，</span>
+                      <span>Material 现已支持 10 种语言</span>
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="item-wrap">
-              <div className="item">
-                <img src="https://alimg.viosey.com/mt/advantage/third-party.jpg" alt="[第三方服务]" />
-                <div className="description">
-                  <h4>第三方服务</h4>
-                  <p>
-                    <span>三款网站统计服务，</span>
-                    <span>六款评论系统</span>
-                  </p>
+              <div className="item-wrap">
+                <div className="item">
+                  <img src="https://alimg.viosey.com/mt/advantage/third-party.jpg" alt="[第三方服务]" />
+                  <div className="description">
+                    <h4>第三方服务</h4>
+                    <p>
+                      <span>三款网站统计服务，</span>
+                      <span>六款评论系统</span>
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="item-wrap">
-              <div className="item">
-                <img src="https://alimg.viosey.com/mt/advantage/infinite.jpg" alt="[一种风格，无限可能]" />
-                <div className="description">
-                  <h4>一种风格，无限可能</h4>
-                  <p>
-                    <span>基于 Material Design，</span>
-                    <span>可自定义的背景与颜色风格</span>
-                  </p>
+              <div className="item-wrap">
+                <div className="item">
+                  <img src="https://alimg.viosey.com/mt/advantage/infinite.jpg" alt="[一种风格，无限可能]" />
+                  <div className="description">
+                    <h4>一种风格，无限可能</h4>
+                    <p>
+                      <span>基于 Material Design，</span>
+                      <span>可自定义的背景与颜色风格</span>
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
           </div>
         </div>
 
